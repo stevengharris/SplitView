0f54a71d90942e52345b45e6556fc24ba9e06e50
Steven G. Harris <steven.g.harris@gmail.com>
2023-02-05 15:53:05 -0800

    Expand arguments for hSplit and vSplit to accept CGFloat and SplitSide without needing SplitFraction and SplitHide.

--- Sources/SplitView/SplitModifiers.swift
+++ Sources/SplitView/SplitModifiers.swift
@@ -43,15 +43,88 @@ public struct VSplit<S: View>: ViewModifier {
 
 extension View {
     
-    /// Return a `SplitView(layout: .Horizontal)` split between the `primary` View (self) and `secondary`.
-    public func hSplit(fraction: SplitFraction? = nil, hide: SplitHide? = nil, @ViewBuilder secondary: @escaping (() -> some View)) -> some View {
+    /// Return a `SplitView(layout: .Horizontal)` split between the `primary` View (self) and `secondary`
+    /// specifying `fraction` as a SplitFraction and `hide` as a SplitHide.
+    public func hSplit(fraction: SplitFraction, hide: SplitHide, @ViewBuilder secondary: @escaping (() -> some View)) -> some View {
         modifier(HSplit(fraction: fraction, hide: hide, secondary: secondary))
     }
     
-    /// Return a `SplitView(layout: .Vertical)` split between the `primary` View (self) and `secondary`.
-    public func vSplit(fraction: SplitFraction? = nil, hide: SplitHide? = nil, @ViewBuilder secondary: @escaping (() -> some View)) -> some View {
+    /// Return a `SplitView(layout: .Horizontal)` split between the `primary` View (self) and `secondary`
+    /// specifying only `fraction` as a SplitFraction.
+    public func hSplit(fraction: SplitFraction, @ViewBuilder secondary: @escaping (() -> some View)) -> some View {
+        modifier(HSplit(fraction: fraction, hide: SplitHide(nil), secondary: secondary))
+    }
+    
+    /// Return a `SplitView(layout: .Horizontal)` split between the `primary` View (self) and `secondary`
+    /// specifying only `hide` as a SplitHide.
+    public func hSplit(hide: SplitHide, @ViewBuilder secondary: @escaping (() -> some View)) -> some View {
+        modifier(HSplit(fraction: SplitFraction(nil), hide: hide, secondary: secondary))
+    }
+    
+    /// Return a `SplitView(layout: .Horizontal)` split between the `primary` View (self) and `secondary`
+    /// specifying `fraction` as a CGFloat and `hide` as a SplitSide (i.e., .Primary or .Secondary).
+    public func hSplit(fraction: CGFloat, hide: SplitSide, @ViewBuilder secondary: @escaping (() -> some View)) -> some View {
+        modifier(HSplit(fraction: SplitFraction(fraction), hide: SplitHide(hide), secondary: secondary))
+    }
+    
+    /// Return a `SplitView(layout: .Horizontal)` split between the `primary` View (self) and `secondary`
+    /// specifying only `fraction` as a CGFloat.
+    public func hSplit(fraction: CGFloat, @ViewBuilder secondary: @escaping (() -> some View)) -> some View {
+        modifier(HSplit(fraction: SplitFraction(fraction), hide: SplitHide(nil), secondary: secondary))
+    }
+    
+    /// Return a `SplitView(layout: .Horizontal)` split between the `primary` View (self) and `secondary`
+    /// specifying only `hide` as a SplitSide (i.e., .Primary or .Secondary).
+    public func hSplit(hide: SplitSide, @ViewBuilder secondary: @escaping (() -> some View)) -> some View {
+        modifier(HSplit(fraction: SplitFraction(nil), hide: SplitHide(hide), secondary: secondary))
+    }
+    
+    /// Return a `SplitView(layout: .Horizontal)` split between the `primary` View (self) and `secondary
+    /// using defaults for `fraction` and `hide`.
+    public func hSplit(@ViewBuilder secondary: @escaping (() -> some View)) -> some View {
+        modifier(HSplit(fraction: SplitFraction(nil), hide: SplitHide(nil), secondary: secondary))
+    }
+    
+    /// Return a `SplitView(layout: .Vertical)` split between the `primary` View (self) and `secondary`
+    /// specifying `fraction` as a SplitFraction and `hide` as a SplitHide.
+    public func vSplit(fraction: SplitFraction, hide: SplitHide, @ViewBuilder secondary: @escaping (() -> some View)) -> some View {
         modifier(VSplit(fraction: fraction, hide: hide, secondary: secondary))
     }
+    
+    /// Return a `SplitView(layout: .Vertical)` split between the `primary` View (self) and `secondary`
+    /// specifying only `fraction` as a SplitFraction.
+    public func vSplit(fraction: SplitFraction, @ViewBuilder secondary: @escaping (() -> some View)) -> some View {
+        modifier(VSplit(fraction: fraction, hide: SplitHide(nil), secondary: secondary))
+    }
+    
+    /// Return a `SplitView(layout: .Vertical)` split between the `primary` View (self) and `secondary`
+    /// specifying only `hide` as a SplitHide.
+    public func vSplit(hide: SplitHide, @ViewBuilder secondary: @escaping (() -> some View)) -> some View {
+        modifier(VSplit(fraction: SplitFraction(nil), hide: hide, secondary: secondary))
+    }
+    
+    /// Return a `SplitView(layout: .Vertical)` split between the `primary` View (self) and `secondary`
+    /// specifying `fraction` as a CGFloat and `hide` as a SplitSide (i.e., .Primary or .Secondary).
+    public func vSplit(fraction: CGFloat, hide: SplitSide, @ViewBuilder secondary: @escaping (() -> some View)) -> some View {
+        modifier(VSplit(fraction: SplitFraction(fraction), hide: SplitHide(hide), secondary: secondary))
+    }
+    
+    /// Return a `SplitView(layout: .Vertical)` split between the `primary` View (self) and `secondary`
+    /// specifying only `fraction` as a CGFloat.
+    public func vSplit(fraction: CGFloat, @ViewBuilder secondary: @escaping (() -> some View)) -> some View {
+        modifier(VSplit(fraction: SplitFraction(fraction), hide: SplitHide(nil), secondary: secondary))
+    }
+    
+    /// Return a `SplitView(layout: .Vertical)` split between the `primary` View (self) and `secondary`
+    /// specifying only `hide` as a SplitSide (i.e., .Primary or .Secondary).
+    public func vSplit(hide: SplitSide, @ViewBuilder secondary: @escaping (() -> some View)) -> some View {
+        modifier(VSplit(fraction: SplitFraction(nil), hide: SplitHide(hide), secondary: secondary))
+    }
+    
+    /// Return a `SplitView(layout: .Vertical)` split between the `primary` View (self) and `secondary
+    /// using defaults for `fraction` and `hide`.
+    public func vSplit(@ViewBuilder secondary: @escaping (() -> some View)) -> some View {
+        modifier(VSplit(fraction: SplitFraction(nil), hide: SplitHide(nil), secondary: secondary))
+    }
+    
 }
-
-
